3573db5f99f11bc0c395398375b39c69cefc3142
Sky Jones <rsajones94@gmail.com>
2020-07-28 17:21:01 -0500

    initial commit

---
+++ scan-reporting/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 17:19:21 2020
+
+@author: manusdonahue
+"""
+
---
+++ scan-reporting/generate_mm_report.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 17:18:10 2020
+
+@author: manusdonahue
+"""
+
